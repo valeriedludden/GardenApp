866c040785782fe7381a5cf6febf36e49c24508a
JacobDeMille <jacob.demille@icloud.com>
2020-10-26 20:13:35 -0600

    Added a new comment

--- app/src/main/java/com/team03/gardenapp/MainActivity.java
+++ app/src/main/java/com/team03/gardenapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //for the commit number 21
+        //for the commit
+        // Comment from Jacob DeMille
     }
 }
