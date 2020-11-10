51a2985a02f410193774b61f917724be8545ae9f
Valerie Ludden <valudden@gmail.com>
2020-11-10 12:18:25 -0700

    commented out BasePlant Activity and Adapter

--- app/src/main/java/com/team03/gardenapp/BasePlantActivity.java
+++ app/src/main/java/com/team03/gardenapp/BasePlantActivity.java
@@ -10,22 +10,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class BasePlantActivity  extends AppCompatActivity {
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-
-    EditText txtName;
-    EditText txtSunshine;
-    EditText txtType;
-
-    BasePlant plant;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_base_plants);
-        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.mDatabaseReference;
-        txtName = (EditText) findViewById(R.id.txtName);
-    }
+//    private FirebaseDatabase mFirebaseDatabase;
+//    private DatabaseReference mDatabaseReference;
+//
+//    EditText txtName;
+//    EditText txtSunshine;
+//    EditText txtType;
+//
+//    BasePlant plant;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list_base_plants);
+//        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+//        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+//        txtName = (EditText) findViewById(R.id.txtName);
+//    }
 
 }
--- app/src/main/java/com/team03/gardenapp/BasePlantAdapter.java
+++ app/src/main/java/com/team03/gardenapp/BasePlantAdapter.java
@@ -24,98 +24,99 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class BasePlantAdapter  extends RecyclerView.Adapter<BasePlantAdapter.BasePlantViewHoler> {
-    ArrayList<BasePlant> bPlants;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildListener;
-
-    public BasePlantAdapter() {
-        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.mDatabaseReference;
-        this.bPlants = FirebaseUtil.mBasePlants;
-        mChildListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                BasePlant bp = dataSnapshot.getValue(BasePlant.class);
-                Log.d("Plant Name = ", bp.getName());
-                bp.setId(dataSnapshot.getKey());
-                bPlants.add(bp);
-                notifyItemInserted(bPlants.size()-1);
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-        mDatabaseReference.addChildEventListener(mChildListener);
-    }
-
-    @Override
-    public BasePlantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        View itemView = LayoutInflater.from(context)
-                .inflate(R.layout.rv_row, parent, false);
-        return new BasePlantViewHolder(itemView);
-
-    }
-
-    @Override
-    public void onBindViewHolder(BasePlantViewHolder holder, int position) {
-        BasePlant plants = plants.get(position);
-        holder.bind(plants);
-    }
-
-    @Override
-    public int getItemCount() {
-        return bPlants.size();
-    }
-
-    public class BasePlantViewHolder extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
-        TextView tvName;
-        TextView tvSunlight;
-        TextView tvLastWatered;
-
-        public BasePlantViewHolder(View itemView) {
-            super(itemView);
-            tvName = (TextView) itemView.findViewById(R.id.tvName);
-            tvSunlight = (TextView) itemView.findViewById(R.id.tvSunlight);
-            tvLastWatered = (TextView) itemView.findViewById(R.id.tvLastWatered);
-            itemView.setOnClickListener(this);
-        }
-
-        public void bind(BasePlant plant) {
-            tvName.setText(plant.getName());
-            tvSunlight.setText(plant.getSunlight());
-            tvLastWatered.setText(plant.getLastWatered());
-        }
-
-        @Override
-        public void onClick(View view) {
-            int position = getAdapterPosition();
-            Log.d("Click", String.valueOf(position));
-            BasePlant selectedPlant = bPlants.get(position);
-            Intent intent = new Intent(view.getContext(), BasePlantActivity.class);
-            intent.putExtra("BasePlant", selectedPlant);
-            view.getContext().startActivity(intent);
-        }
-    }
+public class BasePlantAdapter {
+//public class BasePlantAdapter  extends RecyclerView.Adapter<BasePlantAdapter.BasePlantViewHolder> {
+//    ArrayList<BasePlant> bPlants;
+//    private FirebaseDatabase mFirebaseDatabase;
+//    private DatabaseReference mDatabaseReference;
+//    private ChildEventListener mChildListener;
+//
+//    public BasePlantAdapter() {
+//        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+//        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+//        this.bPlants = FirebaseUtil.mBasePlants;
+//        mChildListener = new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                BasePlant bp = dataSnapshot.getValue(BasePlant.class);
+//                Log.d("Plant Name = ", bp.getName());
+//                bp.setId(dataSnapshot.getKey());
+//                bPlants.add(bp);
+//                notifyItemInserted(bPlants.size()-1);
+//
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        };
+//        mDatabaseReference.addChildEventListener(mChildListener);
+//    }
+//
+//    @Override
+//    public BasePlantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+////        Context context = parent.getContext();
+////        View itemView = LayoutInflater.from(context)
+////                .inflate(R.layout.rv_row, parent, false);
+////        return new BasePlantViewHolder(itemView);
+//
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(BasePlantViewHolder holder, int position) {
+//        BasePlant plants = plants.get(position);
+//        holder.bind(plants);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return bPlants.size();
+//    }
+//
+//    public class BasePlantViewHolder extends RecyclerView.ViewHolder
+//            implements View.OnClickListener {
+//        TextView tvName;
+//        TextView tvSunlight;
+//        TextView tvLastWatered;
+//
+//        public BasePlantViewHolder(View itemView) {
+//            super(itemView);
+//            tvName = (TextView) itemView.findViewById(R.id.tvName);
+//            tvSunlight = (TextView) itemView.findViewById(R.id.tvSunlight);
+//            tvLastWatered = (TextView) itemView.findViewById(R.id.tvLastWatered);
+//            itemView.setOnClickListener(this);
+//        }
+//
+//        public void bind(BasePlant plant) {
+//            tvName.setText(plant.getName());
+//            tvSunlight.setText(plant.getSunlight());
+//            tvLastWatered.setText(plant.getLastWatered());
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+//            int position = getAdapterPosition();
+//            Log.d("Click", String.valueOf(position));
+//            BasePlant selectedPlant = bPlants.get(position);
+//            Intent intent = new Intent(view.getContext(), BasePlantActivity.class);
+//            intent.putExtra("BasePlant", selectedPlant);
+//            view.getContext().startActivity(intent);
+//        }
+//    }
 }
