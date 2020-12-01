8c482fd419134ff9ebe36ffef71ffb3ee155fcd0
JacobDeMille <jacob.demille@icloud.com>
2020-11-30 22:15:00 -0700

    Added floating action button to my plants page

--- app/src/main/java/com/team03/gardenapp/MyPlants.java
+++ app/src/main/java/com/team03/gardenapp/MyPlants.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyPlants extends AppCompatActivity {
 
@@ -24,5 +28,16 @@ public class MyPlants extends AppCompatActivity {
         LinearLayoutManager dealsLayoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rvUserPlants.setLayoutManager(dealsLayoutManager);
+        //Floating Action for add more plants
+        //Button is constrained to the bottom of the page so that it
+        //will stay in place as the user scrolls
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MyPlants.this, AddUserPlant.class));
+            }
+        });
     }
+
 }
