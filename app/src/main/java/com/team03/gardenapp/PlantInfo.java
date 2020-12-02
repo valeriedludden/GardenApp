361269c22e345f9daa18b822c924c40c528d8cdb
JacobDeMille <jacob.demille@icloud.com>
2020-12-01 21:21:19 -0700

    Changed around the UI for the CardView and changed the app color scheme

--- app/src/main/java/com/team03/gardenapp/PlantInfo.java
+++ app/src/main/java/com/team03/gardenapp/PlantInfo.java
@@ -1,14 +1,45 @@
 package com.team03.gardenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PlantInfo extends AppCompatActivity {
+
+    private RecyclerView.ViewHolder viewHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_info);
+
+
+//        public static class ViewHolder extends RecyclerView.ViewHolder {
+//            public View view;
+//            public ClipData.Item currentItem;
+//
+//            public ViewHolder(View v) {
+//                super(v);
+//                view = v;
+//                view.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        //info to display to user
+//                    }
+//                });
+//            }
+//        }
+//            @Override
+//        public void onBindViewHolder(ViewHolder viewHolder, int i) {
+//                viewHolder.currentItem = items.get(i);
+//            }
+//        }
     }
 }
