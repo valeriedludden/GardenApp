bb02adaa45c0acd417012960b90832bade9dddf7
Darrin <productionstagemanager@gmail.com>
2020-12-07 23:41:23 -0700

    Random Tip on Homepage

--- app/src/main/java/com/team03/gardenapp/BottomNavigation.java
+++ app/src/main/java/com/team03/gardenapp/BottomNavigation.java
@@ -10,11 +10,19 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 /**
  * UI for the main app page
@@ -26,15 +34,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  */
 
 public class BottomNavigation extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
+
+        //set variables for daily plant tip. Including the text view and the random number to select the tip.
+        final TextView plantTipView = (TextView) findViewById(R.id.plantTip);
+        final Random random = new Random();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +54,18 @@ public class BottomNavigation extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(BottomNavigation.this, AddUserPlant.class));
             }
+        });
+        //Load the randomly selected plant tip.
+        mDatabase.child("PlantTips").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int count = (int) snapshot.getChildrenCount();
+                String counter = String.valueOf(random.nextInt(count));
+                String tip = snapshot.child(counter).child("Tip").getValue().toString();
+                plantTipView.setText(tip);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
         });
 
     }
